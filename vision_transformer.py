0bd1190e32710d91aa6ae7ccecc8c2099d9b244b
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-04-04 14:34:25 -0500

    Initialization of norm layers

--- vision_transformer.py
+++ vision_transformer.py
@@ -255,7 +255,7 @@ class VisionTransformer(nn.Module):
             trunc_normal_(m.weight, std=.02)
             if isinstance(m, nn.Linear) and m.bias is not None:
                 nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
+        elif isinstance(m, (nn.LayerNorm, nn.BatchNorm1d, nn.GroupNorm,)):
             nn.init.constant_(m.bias, 0)
             nn.init.constant_(m.weight, 1.0)
 
